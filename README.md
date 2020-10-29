389e880300c1d79d177cb7c03b9f5bb83c56fc0f
Matt Hunt <wmatthunt@gmail.com>
2020-10-29 15:35:55 -0400

    made table

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # React-Employee-Directory
 placeholder
+learned how to make table from: https://dev.to/abdulbasit313/an-easy-way-to-create-a-customize-dynamic-table-in-react-js-3igg
--- employee_directory/src/App.js
+++ employee_directory/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import employees from "./data/employees.json";
+import Table from "./components/Table";
+//import employees from "./data/employees.json";
 
-function App ()
-{
+function App() {
     return (
         <div>
             <h1>Employee Directory</h1>
+            <Table />
         </div>
     );
 }
