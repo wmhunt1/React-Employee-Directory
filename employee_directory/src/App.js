6b09d582550bd382eb32162480954fbcb365e3b8
Matt Hunt <wmatthunt@gmail.com>
2020-10-29 16:05:22 -0400

    working on using json instead

--- employee_directory/src/App.js
+++ employee_directory/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Table from "./components/Table";
-//import employees from "./data/employees.json";
+import employees from "./data/employees.json";
 
 function App() {
     return (
