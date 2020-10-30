36c833a340d0e39fee754ed75c068ed03e034f80
Matt Hunt <wmatthunt@gmail.com>
2020-10-30 10:18:23 -0400

    starting over with axios

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # React-Employee-Directory
 placeholder
 learned how to make table from: https://dev.to/abdulbasit313/an-easy-way-to-create-a-customize-dynamic-table-in-react-js-3igg
+https://www.pluralsight.com/guides/load-and-render-json-data-into-react-components
--- employee_directory/src/App.js
+++ employee_directory/src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
-import Table from "./components/Table";
-import employees from "./data/employees.json";
 
 function App() {
     return (
         <div>
             <h1>Employee Directory</h1>
-            <Table />
         </div>
     );
 }
